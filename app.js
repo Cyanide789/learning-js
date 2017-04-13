e920125df035f3d413beaa15f3cb3ef8d09a5a68
cyanide789 <tompiccin@gmail.com>
2017-04-13 17:39:11 +0200

    Created a person object.

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 /* 
   This is an exploratory project to learn JavaScript
+
   @author tom.piccin
 */
+
+// Main logic
+
+var tom = new Person("Tom", 28);
+var msg = tom.toString();
+
+console.log(msg);
+
+// Objects
+
+/**
+ * Creates a new Person object.
+ * 
+ * @param {String} name 
+ * @param {Number} age 
+ */
+function Person(name, age) {
+  
+  this.name = name;
+  this.age = age;
+
+  this.toString = function() {
+    return "name: " + name + ", age: " + age;
+  }
+}

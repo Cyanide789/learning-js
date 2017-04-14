9c80bd5fe65613d5b7a6f53a5b69fa5bab9f0557
cyanide789 <tompiccin@gmail.com>
2017-04-14 14:11:51 +0200

    Added nested Company object to Person object.

--- app.js
+++ app.js
@@ -9,13 +9,17 @@
 // Main logic
 console.log("== start of program ==");
 
+// Create a valid company object
+var miniCorp = new Company("MiniCorp", "MCP");
+
 // Create a valid person object
-var tom = new Person("Tom", 28);
+var tom = new Person("Tom", 28, miniCorp);
+console.log(miniCorp.toString());
 console.log(tom.toString());
 
 // Create an invalid person object
 try {
-    var dries = new Person("Dries", "Dries");
+    var dries = new Person("Dries", "invalid_lastname");
 } catch (error) {
     console.log(error);
 }
@@ -29,14 +33,34 @@ console.log("=== end of program ===");
  *
  * @param {string} name
  * @param {Number} age
+ * @param {Company} company
  */
-function Person(name, age) {
+function Person(name, age, company) {
 
     isString(name) ? this.name = name : _throw(name + " is not a valid name");
     isNumber(age) ? this.age = age : _throw(age + " is not a valid age");
+    company ? this.company = company : null;
 
     this.toString = function () {
-        return "Person, name: " + name + ", age: " + age;
+        var msg = "Person: {name: " + name + ", age: " + age
+        company ? msg += ", " + company.toString() : null;
+        return msg += "}";
+    }
+}
+
+/**
+ * Creates a company object.
+ *
+ * @param {string} name
+ * @param {string} tickerSymbol
+ */
+function Company(name, tickerSymbol) {
+
+    isString(name) ? this.name = name : _throw(name + " is not a valid name");
+    isString(tickerSymbol) ? this.tickerSymbol = tickerSymbol : _throw(name + " is not a valid ticker symbol");
+
+    this.toString = function () {
+        return "Company: {name: " + this.name + ", tickersymbol: " + tickerSymbol + "}";
     }
 }
 
